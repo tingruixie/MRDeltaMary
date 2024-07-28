14ebebcddc7dd2de5a369d2b4e285626ec832860
Michelle Xie <Michelle.Xie111@gmail.com>
2024-07-28 11:17:07 -0500

    Initial Commit

---
+++ MRDeltaMary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MRDeltaMary
+//
+//  Created by Zhifu Xie on 7/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MRDeltaMary/MRDeltaMaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MRDeltaMaryApp.swift
+//  MRDeltaMary
+//
+//  Created by Zhifu Xie on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MRDeltaMaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
